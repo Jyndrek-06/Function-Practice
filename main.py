778f744eb34b67b065b57526efa2528f8d3c9de1
Andrzej Blachuta <113551896+Jyndrek-06@users.noreply.github.com>
2022-11-01 19:11:57 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+#functions printing and returning
+#print something in a function, its only for displaying
+# some data, you are doing nothing with the data
+
+# when you return in a function, you are going to use it
+# in another part of your program 
+
+apples = int(input("How many apples?"))
+oranges = int(input("How many oranges?"))
+
+#add fruit
+
+#subtract fruit
+
+#divide fruit
+
+#disply the added fruit, subtracted fruit, and divided fruit
