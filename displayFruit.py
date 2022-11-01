ad125b06c7a07b10516097d1c26747363a5d0e92
Andrzej Blachuta <113551896+Jyndrek-06@users.noreply.github.com>
2022-11-01 19:38:39 +0000

    We worked on returning values and functions.

---
+++ displayFruit.py
@@ -0,0 +1,2 @@
+def display_fruit(x,y,z):
+  print(f"The added fruit are {x}, the subtracted are {y}, and the divided are {z}")
--- main.py
+++ main.py
@@ -4,14 +4,25 @@
 
 # when you return in a function, you are going to use it
 # in another part of your program 
+from addfruit import add_fruit
+from divide_Fruit import divide_fruit
+from subtract_fruit import subtract_fruit
+from displayFruit import display_fruit
 
 apples = int(input("How many apples?"))
 oranges = int(input("How many oranges?"))
 
 #add fruit
-
-#subtract fruit
-
-#divide fruit
-
-#disply the added fruit, subtracted fruit, and divided fruit
+# When ever you return items, you must put the
+# returned value inside of a variable
+fruitAdded = add_fruit(apples,oranges)
+print(fruitAdded)
+# #subtract fruit
+sub = subtract_fruit(apples,oranges)
+print(sub)
+# #divide fruit
+div = divide_fruit(apples,oranges)
+print(div)
+# #disply the added fruit, subtracted fruit, and divided fruit
+# create another file called display fruit 
+display_fruit(fruitAdded, sub, div)
